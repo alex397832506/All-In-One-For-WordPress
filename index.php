dfbe27318d561ccf79c3759bafe8ec669bc26480
alex397832506 <397832506@qq.com>
2015-12-02 08:55:46 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,12 +8,41 @@ Author: Alex
 Author URI: http://www.atalex.net
 License: GNU GENERAL PUBLIC LICENSE
 */
-?>
 
-<?php
-include_once('updater.php');
- if (is_admin()) { 
-        $config = array(
+/**
+
+ * @package All-In-One-For-WordPress
+ * @author Alex @link http://atalex.net
+ * @since 1.0.1
+ * @version 1.0.1
+ */
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+add_action( 'init', 'github_plugin_updater_test_init' );
+function github_plugin_updater_test_init() {
+
+	include_once 'updater.php';
+
+	define( 'WP_GITHUB_FORCE_UPDATE', true );
+
+	if ( is_admin() ) { // note the use of is_admin() to double check that this is happening in the admin
+
+		$config = array(
             'slug' => plugin_basename( __FILE__ ),
             'proper_folder_name' => 'All-In-One-For-WordPress', // this is the name of the folder your plugin lives in
             'api_url' => 'https://api.github.com/repos/alex397832506/All-In-One-For-WordPress', // the GitHub API url of your GitHub repo
